b9fab382858d25df21371aa8778bee168591d344
Mark Harrell <mark@mjharrell.com>
2014-11-03 23:32:27 -0800

    simple mongo

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOOSEDB);
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({msg: 'yay!'});
+  console.log('got it!');
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Listening on port: ' + port);
